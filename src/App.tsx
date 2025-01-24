b97fa17cea5e6c02b9a2ea5b4d34516d4be0370e
Florence Bastaraud <fbastaraud@yahoo.fr>
2025-01-24 15:14:32 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="wrapper mx-auto text-center">
+      <h1 className="text-6xl">NearMe</h1>
+      <p className="text-lg">
+        Discover the best places and events around you and <br />
+        explore your local area effortlessly while making the most of what's
+        around you!
+      </p>
+    </main>
+  )
+}
+
+export default App
